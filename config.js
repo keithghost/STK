c94e9951e05df2e98a4609f1ff760f109264d70a
keithghost <keithalphamd@gmail.com>
2025-07-01 21:09:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const config = {
     passkey: process.env.PASSKEY || "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
     number: process.env.PUSHNUMBER || "254748387615",
     amount: process.env.AMOUNT || "1",
-    callbackUrl: process.env.CALLBACK_URL || ""
+    callbackUrl: process.env.CALLBACK_URL || "https://stk-1wop.onrender.com"
 };
 
 module.exports = config;

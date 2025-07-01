a828f162ddc36e17fca2ff2112a1580a7030d5f7
keithghost <keithalphamd@gmail.com>
2025-07-01 20:43:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 
 const config = {
-    consumerKey: process.env.CONSUMER_KEY || "",
-    consumerSecret: process.env.CONSUMER_SECRET || "",
+    consumerKey: process.env.CONSUMER_KEY || "zL7LkIMIYGuaIKcG8hXvp0dmtcofUUN2rToWINbBBYqIV4Fh",
+    consumerSecret: process.env.CONSUMER_SECRET || "RoFc8lYb8pP5TGgMjWeIeYtU2QJXWz0z8CqSinPiWAJB0DwphMEDQqSZst0FYoEd",
     shortCode: process.env.SHORTCODE || "174379",
     passkey: process.env.PASSKEY || "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
-    number: process.env.PUSHNUMBER || "254708374149",
+    number: process.env.PUSHNUMBER || "254748387615",
     amount: process.env.AMOUNT || "1",
     callbackUrl: process.env.CALLBACK_URL || ""
 };
